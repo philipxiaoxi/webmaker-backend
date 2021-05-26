3ed0c34f281352a79ccc35d04ff595108aad41d8
DIY熙 <xswork@qq.com>
2021-05-26 19:43:38 +0800

    修改登录错误提示语

--- src/main/java/com/lazyfish/codeshare/controller/LoginControllerAdvice.java
+++ src/main/java/com/lazyfish/codeshare/controller/LoginControllerAdvice.java
@@ -24,15 +24,20 @@ public class LoginControllerAdvice {
         // 判断场景值，定制化异常信息
         String message = "";
         if (nle.getType().equals(NotLoginException.NOT_TOKEN)) {
-            message = "未提供token";
+            //未提供token
+            message = "请重新登录。";
         } else if (nle.getType().equals(NotLoginException.INVALID_TOKEN)) {
-            message = "token无效";
+            //token无效
+            message = "您的认证无效，请重新登录。";
         } else if (nle.getType().equals(NotLoginException.TOKEN_TIMEOUT)) {
-            message = "token已过期";
+            //token已过期
+            message = "您的登录已过期。";
         } else if (nle.getType().equals(NotLoginException.BE_REPLACED)) {
-            message = "token已被顶下线";
+            //token已被顶下线
+            message = "您的账号已在其他地方登录，您已被强制下线。";
         } else if (nle.getType().equals(NotLoginException.KICK_OUT)) {
-            message = "token已被踢下线";
+            //token已被踢下线
+            message = "您被管理员强制下线。";
         } else {
             message = "当前会话未登录";
         }
