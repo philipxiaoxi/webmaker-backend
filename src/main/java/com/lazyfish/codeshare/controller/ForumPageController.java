e1b0c710aa5f2bcd2da4f76d5d15d0e690b53ed8
DIY熙 <xswork@qq.com>
2021-09-04 21:17:11 +0800

    feat 新增论坛页面路由

--- src/main/java/com/lazyfish/codeshare/controller/ForumPageController.java
+++ src/main/java/com/lazyfish/codeshare/controller/ForumPageController.java
@@ -1,17 +1,19 @@
 package com.lazyfish.codeshare.controller;
 
+import cn.dev33.satoken.stp.StpUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.lazyfish.codeshare.entity.ForumPage;
-import com.lazyfish.codeshare.entity.Snippet;
 import com.lazyfish.codeshare.service.ForumPageService;
 import com.lazyfish.codeshare.utils.ResultBuild;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.util.Date;
 import java.util.Map;
 
 @CrossOrigin(origins = "*") // 支持跨域
@@ -32,5 +34,16 @@ public class ForumPageController {
     public ResultBuild getForumPage(Integer id) {
         Map<String, Object> forumPage = forumPageService.getForumPage(id);
         return new ResultBuild(200,forumPage);
+    }
+    @RequestMapping("/api/insertForumPage")
+    @ResponseBody
+    public ResultBuild insertForumPage(@RequestHeader("token") String token, ForumPage forumPage) {
+        String userId = (String) StpUtil.getLoginIdByToken(token);
+        forumPage.setUserId(Integer.parseInt(userId));
+        Date time = new Date();
+        forumPage.setTime(time);
+        forumPage.setUpdateTime(time);
+        forumPageService.insertForumPage(forumPage);
+        return new ResultBuild(200,forumPage.getId());
     }
 }
