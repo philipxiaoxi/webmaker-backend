6ea8eec61b8b47223bc523bb18ecb675048e8f8f
DIY熙 <xswork@qq.com>
2021-09-05 11:32:09 +0800

    feat 新增邮件内容模板器

---
+++ src/main/java/com/lazyfish/codeshare/templetes/HtmlBody.java
@@ -0,0 +1,24 @@
+package com.lazyfish.codeshare.templetes;
+
+public class HtmlBody {
+    public static String getCodeHtmlBody(String code) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("<div style=\"text-align: center;margin: 20px;\">\n" +
+                "    <h1>CodeShare代码分享平台</h1>\n" +
+                "    <hr>\n" +
+                "    <div style=\"text-align: left;\">\n" +
+                "        <p>尊敬的用户，您好！</p>\n" +
+                "        <p>\n" +
+                "            您的验证码是：\n" +
+                "            <span style=\"color:brown;font-size: 3rem;margin: 10px;\">")
+                .append(code).append("</span>\n" +
+                "        </p>\n" +
+                "    </div>\n" +
+                "    <hr>\n" +
+                "    <div style=\"font-size: 0.5rem;float:right;margin-top:20px;\">\n" +
+                "        <p>懒鱼工作室开发团队</p>\n" +
+                "    </div>\n" +
+                "</div>\n");
+        return stringBuilder.toString();
+    }
+}
